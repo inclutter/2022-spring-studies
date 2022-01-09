31443aebad165a25ad7865c66cc29284445f5ddb
inclutter <augustman@knou.ac.kr>
2022-01-09 17:38:34 +0900

    Added about chapter 1 source.

---
+++ src/main/java/com/inclutter/springquickstart/api/board/controller/BoardController.java
@@ -0,0 +1,51 @@
+package com.inclutter.springquickstart.api.board.controller;
+
+import com.inclutter.springquickstart.api.board.model.BoardDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class BoardController {
+
+    public BoardController() {
+        System.out.println("===> BoardCOntroller 생성");
+    }
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello : " + name;
+    }
+
+    @GetMapping("/boards/get")
+    public BoardDTO getBoard() {
+        BoardDTO board = new BoardDTO();
+        board.setSeq(1);
+        board.setTitle("테스트 제목....");
+        board.setWriter("테스터");
+        board.setContent("테스트 내용입니다..........");
+        board.setCreateDate(new Date());
+        board.setCnt(0);
+        return board;
+    }
+
+    @GetMapping("/boards")
+    public List<BoardDTO> getBoardList() {
+        List<BoardDTO> boardList = new ArrayList<BoardDTO>();
+        for (int i = 0; i <= 10; i++) {
+            BoardDTO board = new BoardDTO();
+            board.setSeq(i);
+            board.setTitle("제목" + i);
+            board.setWriter("테스터");
+            board.setContent(i + "번 내용입니다.");
+            board.setCreateDate(new Date());
+            board.setCnt(0);
+            boardList.add(board);
+        }
+        return boardList;
+    }
+
+}
