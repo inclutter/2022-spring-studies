c4c15d939ca75a3d2e3787f2d65edbd0513e518e
inclutter <augustman@knou.ac.kr>
2022-01-09 12:22:03 +0900

    Initial commit

---
+++ src/main/java/com/inclutter/springquickstart/SpringQuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.inclutter.springquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuickStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inclutter/springquickstart/SpringQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inclutter.springquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
